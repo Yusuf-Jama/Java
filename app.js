537c43bb3c1292981e2491bbe95bb82212b1c630
Yusuf <ogvenomassassin@gmail.com>
2022-10-03 08:13:36 -0800

    Java

--- app.js
+++ app.js
@@ -1,16 +1,5 @@
-for (let i = 1 ; i <= 20; ++i) {
-    console.log(i)
+const str = "frontend simplified"
 
-    if (i % 3 === 0 && i % 5 === 0) {
-        console.log(`${i} -> Frontend Simplified`)
-    }
-    else if (i % 3 === 0) {
-        console.log(`${i} -> Frontend`)
-    }
-    else if (i % 5 === 0) {
-        console.log(`${i} -> Simplified`)
-    }
-    else {
-        console.log(`${i} -> ${i}`)
-    }
+for (let i = 0; i < str.length; ++i){
+    console.log(str[i])
 }
