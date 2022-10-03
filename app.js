0335761efe7f0269bf8dd21fea60c7fe3795fc96
Yusuf <ogvenomassassin@gmail.com>
2022-10-03 07:48:03 -0800

    Java

--- app.js
+++ app.js
@@ -1,17 +1,16 @@
-//let count = 1
-
-//while (count <= 99) {
-//    count = count + 1
-//    console.log(count)
-//}
-
 for (let i = 1 ; i <= 20; ++i) {
     console.log(i)
 
-    if (i % 3 === 0) {
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log(`${i} -> Frontend Simplified`)
+    }
+    else if (i % 3 === 0) {
         console.log(`${i} -> Frontend`)
     }
-    if (i % 5 === 0) {
+    else if (i % 5 === 0) {
         console.log(`${i} -> Simplified`)
+    }
+    else {
+        console.log(`${i} -> ${i}`)
     }
 }
