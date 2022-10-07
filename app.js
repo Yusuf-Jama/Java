5e7bef6f699857df35842729ddf69c965b599bb6
Yusuf <ogvenomassassin@gmail.com>
2022-10-07 11:54:11 -0800

    Java

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
-function sumOfTwoNumbers(num1, num2){
-    return num1 - num2
-}
+let arr = [20, 30, 40, 50, 100]
 
-console.log(sumOfTwoNumbers(10, 25))
+console.log(arr[0])
+console.log(arr[arr.length-1])
+
+arr.push(200)
+
+console.log(arr)
